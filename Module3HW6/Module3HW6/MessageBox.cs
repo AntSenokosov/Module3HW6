f24bc540a02f552403f09bffb4870d7aa8f3b157
AntSenokosov <a.y.senokosov@gmail.com>
2021-12-21 19:59:19 +0200

    Add method OpenAsync

---
+++ Module3HW6/Module3HW6/MessageBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Module3HW6
+{
+    public class MessageBox
+    {
+        public async void OpenAsync()
+        {
+            Console.WriteLine("Окно открыто");
+            Thread.Sleep(3000);
+        }
+    }
+}
