b10247b985c7f42af7ddd9e3c51b7027785d5397
AntSenokosov <a.y.senokosov@gmail.com>
2021-12-21 20:52:29 +0200

    Add realization in Main

--- Module3HW6/Module3HW6/MessageBox.cs
+++ Module3HW6/Module3HW6/MessageBox.cs
@@ -9,10 +9,24 @@ namespace Module3HW6
 {
     public class MessageBox
     {
+        private Random _random = new Random();
+        public Action<State> Action { get; set; }
         public async void OpenAsync()
         {
             Console.WriteLine("Окно открыто");
-            Thread.Sleep(3000);
+            await Task.Delay(3000);
+            Console.WriteLine("Окно было закрыто пользователем");
+
+            var randomState = _random.Next(0, 1);
+
+            if (randomState == 0)
+            {
+                Action.Invoke(State.Ok);
+            }
+            else
+            {
+                Action.Invoke(State.Cancel);
+            }
         }
     }
 }
--- Module3HW6/Module3HW6/Program.cs
+++ Module3HW6/Module3HW6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Module3HW6
 {
@@ -6,6 +7,32 @@ namespace Module3HW6
     {
         static void Main(string[] args)
         {
+            var message = new MessageBox();
+            var tsk = new TaskCompletionSource();
+
+            message.Action += (state) =>
+            {
+                // var msg = state != State.Ok ? "Ok - операция была подтверджена" : "Cancel - операция была отклонена";
+                // Console.WriteLine(msg);
+                CloseWindow(state);
+
+                tsk.SetResult();
+            };
+
+            message.OpenAsync();
+            tsk.Task.GetAwaiter().GetResult();
+        }
+
+        private static void CloseWindow(State state)
+        {
+            if (state == State.Ok)
+            {
+                Console.WriteLine("Ok - операция была подтверджена");
+            }
+            else
+            {
+                Console.WriteLine("Cancel - операция была отклонена");
+            }
         }
     }
 }
